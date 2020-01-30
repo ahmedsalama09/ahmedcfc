597fd9e85cee13cc61204a145f9d37b6ca2b7749
ahmedsalama09 <60046276+ahmedsalama09@users.noreply.github.com>
2020-01-30 23:08:50 +0200

    Sprints-project

--- main.c
+++ main.c
@@ -1,78 +1,43 @@
-/*
- * GccApplication1.c
- *
- * Created: 17-Jan-20 1:12:11 PM
- * Author : ahmed
- */ 
 
-#include <avr/io.h>
 #include "registers.h"
-#include "std_types.h"
-#include "gpio.h"
-#include "softwaredelay.h"
+#include "SwICU.h"
 #include "led.h"
-#include "sevenSeg.h"
-#include "req9.h"
 #include "pushButton.h"
+#include "dcMotor.h"
+#include "HwPWM.h"
 
-/*INFOS PLEASE READ:
-the requirements from 7:9 are blow and they're commented
-I HAVE A PROBLEM WITH SIMULATION TIMING>> simulator isn't working fine with it, it makes undefined behaviour
-*/
+#define moto_freq 100000
 
 int main(void)
 {
-
+	uint8_t u8_swIcuDistance = 0;
+	uint8_t u8_dutyCycle = 8;
+	uint8_t u8_obstacle;
 	
-	/*REQ9 Example
-	while(1){
-	for(u8_stateloop=0;u8_stateloop<4;u8_stateloop++){
-	app_Ledstate(u8_stateloop);
-	delay_ms(1000);
-	}
-	u8_stateloop=0;
-	}
-	*/
+	MotorDC_Init(MOT_1);
+	MotorDC_Init(MOT_2);
+	MotorDC_Dir(MOT_1, FORWARD);
+	MotorDC_Dir(MOT_2, FORWARD);
+	
+	SwICU_Init(SwICU_EdgeRisiging);
 
-    while (1) 
-    {
-		/*REQ7 Example @ 7segment*
-		uint8_t u8_counter;
-		uint32_t u32_delay;
-		while(u8_counter <= 99){
-			
-			sevenSegEnable(SEG_0);
-			sevenSegWrite(SEG_0,((u8_counter/10)%10));
-			delay_ms(1);
-			sevenSegDisable(SEG_0);
-			
-			sevenSegEnable(SEG_1);
-			sevenSegWrite(SEG_1,((u8_counter/10)%10));
-			delay_ms(1);
-			sevenSegDisable(SEG_1);
-			u32_delay++;
-			
-			if(u32_delay % 469 == 0)
-				u8_counter++;
-		}*/
+
+	HwPWMInit();
+	HwPWMSetDuty(u8_dutyCycle, moto_freq);
+
+	while(1)
+	{
+		timer2DelayMs(300);
+		/*read the obstacle measures*/
+		u8_obstacle = ultrasonic_read();
 		
-		/* REQ8 Example @ LEDS BLINKING*/
-		/*uint8_t u8_loop1,u8_loop2,u8_nobounce=0;
-		if(pushButtonGetStatus(BTN_0) == Pressed){
-			while(pushButtonGetStatus(BTN_0) != Released);
-			gpioPinWrite(GPIOA,BIT0,HIGH);
-			for(u8_loop1=0;u8_loop1<1000;u8_loop1++){
-				for(u8_loop2=0;u8_loop2<2290;u8_loop2++){
-					
-					if((u8_nobounce < 1) && (pushButtonGetStatus(BTN_0) == Pressed)){
-						delay_ms(1000);
-						u8_nobounce++;}
-				}
-			}
-		gpioPinWrite(GPIOA,BIT0,LOW);
-		
-		}*/
-			
+		if(u8_obstacle > 5){
+			MotorDC_Dir(MOT_1, STOP);
+			MotorDC_Dir(MOT_2, STOP);
+		}else
+		{
+			MotorDC_Dir(MOT_1, FORWARD);
+			MotorDC_Dir(MOT_2, FORWARD);
+		}
 	}
 }
-
