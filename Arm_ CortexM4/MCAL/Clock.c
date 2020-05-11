2162f4d0e1c966b0d80fe667544fe08377e2710e
ahmedsalama09 <60046276+ahmedsalama09@users.noreply.github.com>
2020-05-11 19:52:19 +0200

    Modified Clock module

--- Arm_ CortexM4/MCAL/Clock.c
+++ Arm_ CortexM4/MCAL/Clock.c
@@ -2,22 +2,45 @@
 */
 
 /*includes*/
-#include "../std_types.h"
+//#include "_stdint.h"
 #include "Clock.h"
 #include "Clocklcfg.h"
 #include "PerRegs.h"
 
 /*Defines*/
 #define MAX_CLOCK_MHZ (168)
-#define RCC_HSEREADY    (0x20000)
-#define RCC_HSEON   (0x10000)
-#define RCC_CSS_ON  (0x80000)
-#define RCC_HSION   (0x01)
-#define RCC_HSIREADY (0x02)
-/*P = 4 and M =2*/
-#define PLL_P_FACTOR (0x00010000)
-#define PLL_M_FACTOR (0x00000002)
-#define PLL_MAX_N_FACTOR (511)
+#define MIN_CLOCK_MHZ (50)
+
+/*CR Defines*/
+#define RCC_HSEREADY    (0x00010000u)
+#define RCC_HSEON   	(0x00020000u)
+#define RCC_CSS_ON  	(0x80000000u)
+#define RCC_HSION    	(0x00000001u)
+#define RCC_HSIREADY 	(0x00000002u)
+#define RCC_PLL_READY    (0x02000000u)
+#define RCC_PLL_ON 	 	(0x01000000u)
+#define RCC_PLLI2S_ON   (0x04000000u)
+#define RCC_PLLSAI_ON   (0x10000000u)
+
+/*Clock Source Macro*/
+#define RCC_SW_HSI (0x00000000u)
+#define RCC_SW_HSE (0x00000001u)
+#define RCC_SW_PLL (0x00000002u)
+#define RCC_SW_MASK (0x00000003u)
+
+/*PLL Factors*/
+#define PLL_P_MASK 	 (0x00030000u)
+#define PLL_M_MASK	 (0x0000003Fu)
+#define PLL_N_MASK   (0x00007FC0u)
+
+#define PLL_P_FACTOR (0x00000000u)
+
+#define PLL_M_FACTOR (0x00000008u)
+#define PLL_MAX_N_FACTOR (511u)
+
+#define PLL_N_FACTOR_MASK (0x1FFu)
+#define PLL_N_FACTOR_SHIFT (6u)
+#define PLL_N_MULTIPLICATION_FACTOR (2u)
 
 #define PLL_HSE_SOURCE (0x400000)
 
@@ -26,10 +49,24 @@
  
 #define RCC_PLL_SYSCLOCK (0x02)
 
+/*PLLCFGR source Macros*/
+#define RCC_PLLSOURCE_MASK 	 (0x00400000u)
+#define RCC_PLLSOURCE_HSE	 (0x00400000u)
+#define RCC_PLLSOURCE_HSI	 (0x00000000u)
+
+/*Buses Macros*/
+#define RCC_CFGR_AHBPSC_MASK (0x000000F0u)
+#define RCC_CFGR_PRE1_MASK   (0x00001C00u)
+#define RCC_CFGR_PRE2_MASK   (0x0000E000u)
+
+/*Buses Prescale*/
+#define RCC_CFGR_AHBPSC		 (0x00000000u)
+#define RCC_CFGR_PB1PSC		 (0x00001400u)
+#define RCC_CFGR_PB2PSC		 (0x00008000u)
+
 /*Buses Prescal*/
 #define RCC_APB2_90MHZ_FACTOR (0x04)
 #define RCC_APB1_45MHZ_FACTOR (0x05)
-
 
 /*APIS*/
 
@@ -44,61 +81,79 @@ ERROR_STATUS Clock_init(void)
     ERROR_STATUS ERR_STATE = E_OK;
 
     /*Check for Clock Value*/
-    if(str_ck_init.Clock_Frequency_MHZ > ((uint32_t) MAX_CLOCK_MHZ))
+    if((str_ck_init.Clock_Frequency_MHZ > ((uint32_t) MAX_CLOCK_MHZ))  || (str_ck_init.Clock_Frequency_MHZ < ((uint32_t) MIN_CLOCK_MHZ)))
     {
         /*Record error*/
         ERR_STATE = ERROR_SYSTICK_BASE + ERROR_WRONG_CLOCK_VALUE;
     }
     else{
 
-        /*Create N Factor Var*/
-        uint16_t PLL_N_Factor;
+    	/*Create NFACTOR Var*/
+    	uint16_t PllN_Factor = 0;
 
-        /*Clear CR REG first*/
-        RCC->CR = ZERO;
+    	/*First enable HSI Clock to replace HSE*/
+    	RCC->CR |= RCC_HSION;
 
-        /*Enable HSE and HSI*/
-        RCC->CR |= (uint32_t) RCC_HSION;
-        while((RCC->CR & ((uint32_t)RCC_HSIREADY)) == ZERO);
+    	/*Wait For HSI To be Ready*/
+    	while((RCC->CR & RCC_HSIREADY) == 0);
 
-        /*wait for RCC Ready Flag*/
-        RCC->CR |= (uint32_t) RCC_HSEON;
-        while((RCC->CR & ((uint32_t)RCC_HSEREADY)) == ZERO);
+    	/*Clear SW BITS*/
+    	RCC->CFGR &= ~RCC_SW_MASK;
 
-        /*Enable CSS*/
-        RCC->CR |= RCC_CSS_ON;
+    	/*Choose HSI To be Clock Source*/
+    	RCC->CFGR |= RCC_SW_HSI;
 
-        /*Set PLL Parameters*/
-        /*P=4 Factor and M = 2*/
-        RCC->PLLCFGR = ZERO;
-        RCC->PLLCFGR |= (((uint32_t)PLL_P_FACTOR) | ((uint32_t)PLL_M_FACTOR)); 
+    	/*Enable HSE*/
+    	RCC->CR |= RCC_HSEON;
 
-        /*Calculate N Factor*/
-        PLL_N_Factor = (8 * str_ck_init.Clock_Frequency_MHZ)/ 8;
+    	/*Wait for HSE To be Ready*/
+    	while((RCC->CR & RCC_HSEREADY) == 0);
 
-        if(PLL_N_Factor > PLL_MAX_N_FACTOR)
-        {
-            ERR_STATE = ERROR_SYSTICK_BASE +  ERROR_WRONG_N_FACTOR;
-        }
-        else
-        {
-            /* assign  N_Fact value */
-            RCC->PLLCFGR |= (PLL_N_Factor << 6);
-        }
+    	/*Disable All PLLs*/
+    	RCC->CR &= ~(RCC_PLL_ON);
+    	RCC->CR &= ~(RCC_PLLSAI_ON);
+    	RCC->CR &= ~(RCC_PLLI2S_ON);
 
-        /*Choose HSE AS PLL Source*/
-        RCC->PLLCFGR |= (uint32_t)PLL_HSE_SOURCE;
-        
-        /*Enable PLL and wait till it's enabled*/
-        RCC->CR |= (uint32_t) PLL_ON;
-        while((RCC->CR & PLL_READY) == ZERO);
+    	/*Configure PLL Parameters
+    	 * P Fixed = 4 , M Fixed = 2
+    	 * N = FPLL OUT
+    	 */
+    	RCC->CFGR &= ~PLL_P_MASK;
+    	RCC->CFGR |= PLL_P_FACTOR;
 
-        /*Choose PLL As Sys Clock*/
-        RCC->CFGR |= (uint32_t) RCC_PLL_SYSCLOCK;
+    	RCC->CFGR &= ~PLL_M_MASK;
+    	RCC->CFGR |= PLL_M_FACTOR;
 
-        /*SET APB1,APB2 Speeds*/
-        RCC->CFGR |= (uint32_t)((RCC_APB1_45MHZ_FACTOR << 10) | (RCC_APB2_90MHZ_FACTOR << 13));
+    	PllN_Factor = PLL_N_MULTIPLICATION_FACTOR * str_ck_init.Clock_Frequency_MHZ;
 
+    	PllN_Factor = PllN_Factor & PLL_N_FACTOR_MASK;
+
+    	/*Asign the value to Reg*/
+    	RCC->PLLCFGR &= ~PLL_N_MASK;
+    	RCC->PLLCFGR |= (PllN_Factor << PLL_N_FACTOR_SHIFT);
+
+    	/*Configure Buses Clocks*/
+    	RCC->CFGR &= ~ RCC_CFGR_AHBPSC_MASK;
+    	RCC->CFGR |= RCC_CFGR_AHBPSC;
+
+    	RCC->CFGR &= ~RCC_CFGR_PRE1_MASK;
+    	RCC->CFGR |= RCC_CFGR_PB1PSC;
+
+    	RCC->CFGR &= ~RCC_CFGR_PRE2_MASK;
+    	RCC->CFGR |= RCC_CFGR_PB2PSC;
+
+    	/*Chooose HSE As PLL Source*/
+    	RCC->PLLCFGR &= ~ RCC_PLLSOURCE_MASK;
+    	RCC->PLLCFGR |= RCC_PLLSOURCE_HSE;
+
+    	/*Enable PLL*/
+    	RCC->CR |= RCC_PLL_ON;
+
+    	/*wait for pll to be ready*/
+    	while((RCC->CR & RCC_PLL_READY) == 0);
+
+    	/*Choose PLL As Sys Clock*/
+    	RCC->CFGR |= RCC_SW_PLL;
     }
 
     return ERR_STATE;
